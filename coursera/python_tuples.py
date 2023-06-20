be4c6550b01fb1c0b85b03a9cab9354df17c1769
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-06-20 22:17:33 +0000

    Started python for coursera

---
+++ coursera/python_tuples.py
@@ -0,0 +1,15 @@
+my_tuple = (1, 'strings', 4.5, True)
+print(type(my_tuple[1]))
+
+set_a = {1, 2, 3, 4, 5}
+
+set_b = {4, 5, 6, 7, 8}
+
+# set methods add, subtract, remove/discard
+# more methods = union( | ), intersection( & ), difference( - ), symmetrical_difference( ^ )
+
+print(set_a & set_b)
+
+# Lists are ordered like and array but same data type, tuples are like lists and arrays with multiple data types.
+# sets are like an array or list of items but are not indexed.
+# dictionary like an object
