bd3bb7fbf821ddd8a1d0d27d2051195bf9aa0734
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-06-20 20:26:21 +0000

    First version roman converter.

--- leet-code/numbers.test.js
+++ leet-code/numbers.test.js
@@ -1,8 +1,68 @@
 import { romanToInt } from './roman_to_int';
 
 describe('romanToInt', () => {
-  it('converts the first numberal', () => {
-    const firstNumeral = 'I';
-    expect(romanToInt(firstNumeral)).toEqual(1);
+  it('converts the first numeral', () => {
+    const numeral = 'I';
+    expect(romanToInt(numeral)).toEqual(1);
+  });
+
+  it('converts the second numeral', () => {
+    const numeral = 'II';
+    expect(romanToInt(numeral)).toEqual(2);
+  });
+
+  it('converts the third numeral', () => {
+    const numeral = 'III';
+    expect(romanToInt(numeral)).toEqual(3);
+  });
+
+  it('converts the fourth numeral', () => {
+    const numeral = 'IV';
+    expect(romanToInt(numeral)).toEqual(4);
+  });
+
+  it('converts the fith numeral', () => {
+    const numeral = 'V';
+    expect(romanToInt(numeral)).toEqual(5);
+  });
+
+  it('converts the sith numeral', () => {
+    const numeral = 'VI';
+    expect(romanToInt(numeral)).toEqual(6);
+  });
+
+  it('converts the seventh numeral', () => {
+    const numeral = 'VII';
+    expect(romanToInt(numeral)).toEqual(7);
+  });
+
+  it('converts the eigth numeral', () => {
+    const numeral = 'VIII';
+    expect(romanToInt(numeral)).toEqual(8);
+  });
+
+  it('converts the ninth numeral', () => {
+    const numeral = 'IX';
+    expect(romanToInt(numeral)).toEqual(9);
+  });
+
+  it('converts the tenth numeral', () => {
+    const numeral = 'X';
+    expect(romanToInt(numeral)).toEqual(10);
+  });
+
+  it('converts the fourteenth numeral', () => {
+    const numeral = 'XIV';
+    expect(romanToInt(numeral)).toEqual(14);
+  });
+
+  it('converts the 20th numeral', () => {
+    const numeral = 'XX';
+    expect(romanToInt(numeral)).toEqual(20);
+  });
+
+  it('converts the 31st numeral', () => {
+    const numeral = 'XXXI';
+    expect(romanToInt(numeral)).toEqual(31);
   });
 });
--- leet-code/roman_to_int.js
+++ leet-code/roman_to_int.js
@@ -1,3 +1,21 @@
 export function romanToInt(numeral) {
-  return 1;
+  const translation = {
+    I: 1,
+    V: 5,
+    X: 10,
+  };
+
+  const letters = numeral.split('');
+
+  let acc = 0;
+
+  letters.forEach((e, i, a) => {
+    if (translation[e] > translation[a[i - 1]]) {
+      acc = translation[e] - acc;
+    } else {
+      acc = acc + translation[e];
+    }
+  });
+
+  return acc;
 }
