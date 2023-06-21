03c5536858c67f126d3ab7cfc128f5f0f290246c
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-06-21 19:37:42 +0000

    Coursera Ordering system

--- coursera/python_data_types.py
+++ coursera/python_data_types.py
@@ -13,3 +13,22 @@ print(set_a & set_b)
 # Lists are ordered like and array but same data type, tuples are like lists and arrays with multiple data types.
 # sets are like an array or list of items but are not indexed.
 # dictionary like an object
+
+#using args to pass multiple variables in a function
+def sum_of(*args):
+  sum = 0
+  for x in args:
+    sum += x
+  return sum
+
+print(sum_of(4, 5, 6))
+
+#kwargs are key values arguments
+
+def sum_of(**kwargs):
+  sum = 0
+  for key, value in kwargs.items():
+    sum += value
+  return round(sum, 2)
+
+print(sum_of(coffee=2.99, cake=4.55, juice=2.99))
