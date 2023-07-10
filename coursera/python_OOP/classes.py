95ad2f5df44ea5d0ead3c71885e1727173769589
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-07-10 19:46:12 +0000

    Second example of creating classes with a method that takes an arguement and returns a value.

--- coursera/python_OOP/classes.py
+++ coursera/python_OOP/classes.py
@@ -6,3 +6,21 @@ class MyClass:
 
 myc = MyClass()
 myc.hello()
+
+# second example
+
+class House:
+    '''
+    Base house Object with rooms and bathrooms.
+    '''
+    num_rooms = 5
+    bathrooms = 2
+
+    def calc_cost(self, rate):
+        #functionality to calc room cost
+        cost = rate * self.num_rooms
+        return cost
+
+my_house = House()
+print('My house has', my_house.num_rooms, 'rooms.')
+print('The rate of rooms is', my_house.calc_cost(3.75))
